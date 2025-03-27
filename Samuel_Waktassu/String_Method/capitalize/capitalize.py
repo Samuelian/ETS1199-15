dcdb7e41076a4d65f1f274beca2ff8d09fd43327
Samuel <125726842+Samuelian@users.noreply.github.com>
2025-03-27 22:43:12 +0300

    Delete Samuel_Waktassu/String_Method/capitalize/capitalize.py

--- Samuel_Waktassu/String_Method/capitalize/capitalize.py
+++
@@ -1,4 +0,0 @@
-print("hello world".capitalize())  # Output: "Hello world"
-print("PYTHON".capitalize())       # Output: "Python"
-print("123abc".capitalize())       # Output: "123abc" (Numbers remain unchanged)
-print("hElLo".capitalize())        # Output: "Hello"
