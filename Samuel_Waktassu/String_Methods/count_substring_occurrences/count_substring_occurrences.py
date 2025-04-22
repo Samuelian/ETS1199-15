6b1e446df19662609e65bae3e00004f56364ff29
Samuel <125726842+Samuelian@users.noreply.github.com>
2025-04-22 20:14:29 +0300

    Create count_substring_occurrences.py

---
+++ Samuel_Waktassu/String_Methods/count_substring_occurrences/count_substring_occurrences.py
@@ -0,0 +1,6 @@
+`count_substring_occurrences(text, substring)`
+
+### **Python Code:**
+```python
+def count_substring_occurrences(text: str, substring: str) -> int:
+    return text.count(substring)
