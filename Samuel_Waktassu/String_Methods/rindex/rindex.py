cd58eef8f381eebb9668a0a7a47db58de63ce96e
Samuel <125726842+Samuelian@users.noreply.github.com>
2025-04-26 22:36:16 +0300

    Create rindex.py

---
+++ Samuel_Waktassu/String_Methods/rindex/rindex.py
@@ -0,0 +1,10 @@
+# rindex_example.py
+
+# Example of str.rindex()
+
+text = "abcabcabc"
+
+# rindex() finds the last (rightmost) occurrence of a substring
+index = text.rindex("a")
+
+print(index)  # Output: 6
