d2df90b8c13678038eee0eec18b8c12a2f26485a
Samuel <125726842+Samuelian@users.noreply.github.com>
2025-04-26 22:33:02 +0300

    Create partition.py

---
+++ Samuel_Waktassu/String_Methods/partition/partition.py
@@ -0,0 +1,4 @@
+text = "Hello, world!"
+# partition() splits into a 3-part tuple: (before separator, separator, after separator)
+result_partition = text.partition(",")
+print("partition():", result_partition)  # Output: ('Hello', ',', ' world!')
