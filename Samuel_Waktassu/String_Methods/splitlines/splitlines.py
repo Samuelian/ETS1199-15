9cee52925e7fcb878431916ca936c9235a502012
Samuel <125726842+Samuelian@users.noreply.github.com>
2025-04-26 22:41:07 +0300

    Create splitlines.py

---
+++ Samuel_Waktassu/String_Methods/splitlines/splitlines.py
@@ -0,0 +1,12 @@
+# splitlines_example.py
+
+# Example of str.splitlines()
+
+text = """Line 1
+Line 2
+Line 3"""
+
+# splitlines() splits text at line breaks into a list
+lines = text.splitlines()
+
+print(lines)  # Output: ['Line 1', 'Line 2', 'Line 3']
